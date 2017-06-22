5313f328971cb4b2c2acb2f432703fb1d93d9376
Edgar Juarez <ejjt.686@gmail.com>
2017-06-21 17:37:30 -0700

    Hovering over links now triggers script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function hightlightLink() {
+    console.log('Highlight!');
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', hightlightLink));
